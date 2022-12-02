89a038e0fe84cd198533555b494730d0cda060b7
Ro <102540777+rockyta07@users.noreply.github.com>
2022-12-02 11:58:20 +0100

    Add files via upload

---
+++ CodigoArduino/horno_1.ino
@@ -0,0 +1,72 @@
+#include <EasyNextionLibrary.h>
+#include <trigger.h>
+#include <Adafruit_MLX90614.h>
+
+#define pinRele 4
+
+///////// Para probar sin horno /////////
+#define TEMP  60 //En grados
+/////////////////////////////////////////
+
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+EasyNex myNex(Serial);
+
+struct Programa 
+{
+  String nombre;
+  int curva[10][2]; //[Instante][Temperatura Deseada, -Grados/min]
+  
+};
+
+
+int temperaturaSensor;
+int temperaturaDeseada;
+
+
+void hornear(int temperaturaSel) {
+  temperaturaDeseada = temperaturaSel;
+  //temperaturaSensor = (int) mlx.readObjectTempC();
+  temperaturaSensor = TEMP;
+  if (temperaturaSensor < temperaturaSel) digitalWrite(pinRele, LOW); //Apagamos la resistencia
+  else if (temperaturaSensor >= temperaturaSel - 5) digitalWrite(pinRele, HIGH); //Encendemos la resistencia 
+
+  Serial.print("HORNO EN FUNCIONAMIENTO, temperatura objetivo: ");
+  Serial.print(temperaturaSel);
+  Serial.print(" Temperatura actual: ");
+  Serial.print(temperaturaSensor);
+  Serial.println();
+}
+
+
+void setup() {
+  myNex.begin(9600);
+  pinMode(pinRele, OUTPUT); //Rele
+  digitalWrite(pinRele, HIGH); //Abrimos el rele por seguridad
+
+  Serial.begin(9600);
+
+  if (!mlx.begin()) {
+    Serial.println("Error de conexion.");
+    while (1);
+  };
+
+  Programa fibra;
+
+  // Declaramos los valores de la curva de FIBRA //
+
+  fibra.nombre = "fibra";
+  for(int i = 0; i < 10; i++){}
+
+}
+
+void loop() {
+  //temperaturaSensor = (int) mlx.readObjectTempC();
+  temperaturaSensor = TEMP;
+  myNex.NextionListen();
+  myNex.writeNum("n1.val", (uint32_t) temperaturaSensor);
+  temperaturaDeseada = myNex.readNumber("n0.val");
+  hornear(temperaturaDeseada);
+
+
+
+}
