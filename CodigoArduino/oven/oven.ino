6bbf28b2660f1bcac56f04f6251f3252551687d8
Rubén <ruben.jimenezmejias@gmail.com>
2023-04-08 13:38:36 +0200

    Add files via upload

--- CodigoArduino/oven/oven.ino
+++ CodigoArduino/oven/oven.ino
@@ -24,6 +24,7 @@ EasyNex display(Serial); // Display
 /*
 sw0.val -> ON/OFF TOGGLE
 n2.val -> Minutes Left
+cb0.val -> Mode
 
 */
 
@@ -44,10 +45,10 @@ struct curve { // Name and Array of time/temperature points to follow
 auto timer = timer_create_default();  // auto means deduce variable type
 
 int sensor_celsius;
-int target_celsius;
-long minutes_left;
 int mode = 0; // 0: manual,
 int programaSeleccionado; // TODO REMOVE, EACH PROGRAM IS A MODE. SIMPLER.
+
+curve biaxial;
 
 ////////////////////////////////// FUNCTIONS //////////////////////////////////
 void set_heater(int target_celsius) {
@@ -77,11 +78,11 @@ void temp_watchdog(int celsius) {
     digitalWrite(RELAY_PIN, RELAY_OFF);
     delay(1000);
   } else if (celsius == NAN){
-    exception("Fallo en el sensor de temperatura");
+    exception("Fallo en el sensor de temperatura",false);
   }
 }
 
-void exception(String error_string) {
+void exception(String error_string, bool stop) {
   /*
   Prints error_string in the nextion display
   */
@@ -89,15 +90,18 @@ void exception(String error_string) {
   display.writeStr("t1.txt", error_string);
   digitalWrite(RELAY_PIN, RELAY_OFF);
   delay(1000);
+  if (stop) {
+    while (1);
+  }
+  
 }
-
 
 void manual_mode() {
   /*
   Updates the time variable in code and the display
-  TODO SEPARATE minutes_left--; from this function?
+  TODO SEPARATE minutes_left--; from this function, so this one can go fast?
   */
-  minutes_left = display.readNumber("n2.val");
+  static long minutes_left = display.readNumber("n2.val");
   minutes_left--;
 
   if (minutes_left == 0){
@@ -109,17 +113,44 @@ void manual_mode() {
   } else {
     // STILL WORK TO DO
     display.writeNum("n2.val", minutes_left);
-    target_celsius = display.readNumber("n0.val");
+    int target_celsius = display.readNumber("n0.val");
     set_heater(target_celsius);
   }
 }
 
 void curve_mode() {
   /*
-  Starts reading the 
+  Starts reading the curve and going linearly from point to point as time goes on.
+  TODO PASS A CURVE TO MAKE THIS FUNCTION GENERIC, instead of using biaxial directly
   */
+  static long minutes_passed = 0;
 
+  // If time passed > than time of final point
+  if (minutes_passed > biaxial.point[biaxial.points - 1].minutes) {
+    // TIME ENDED.
+    digitalWrite(RELAY_PIN, RELAY_OFF); // Heater OFF
+    display.writeNum("sw0.val", 0); // Display toggle OFF
+    timer.cancel(); // End all the tasks of the timer
+  } else {
+    // Still work to do
 
+    // Find the next point to follow
+    int i = 0;
+    while (biaxial.point[i].minutes < minutes_passed) {
+      // When the point is in the future, exit the while loop
+      i++;
+    }
+
+    if (i == 0) {
+      exception("curve_mode ERROR. Either the code or the curve are wrong.", true);
+    }
+
+    // Now i is the next point
+    int target_celsius = interpolate(minutes_passed, biaxial.point[i-1].minutes, biaxial.point[i-1].celsius, biaxial.point[i].minutes, biaxial.point[i].celsius);
+    // TODO write minutes_passed or graph, GUI.
+    set_heater(target_celsius);
+  }
+  minutes_passed++;
 }
 
 void setup() {
@@ -129,25 +160,26 @@ void setup() {
 
   Serial.begin(9600);
 
-  if (!temp_sensor.begin()) exception("No se ha podido inicializar el sensor de temperatura");
+  if (!temp_sensor.begin()) exception("No se ha podido inicializar el sensor de temperatura", true);
 
-  sensor_celsius = (int)temp_sensor.readObjectTempC();
-  // Define curves
-  curve biaxial;
-  biaxial.name = "Biaxial: 85ºC 1.5h, 120ºC 1h";
-  biaxial.points = 4;
+  ////////// CURVES ///////////
+  biaxial.name = "Biaxial: 2ºC/min, 85ºC 1.5h, 120ºC 1h";
+  biaxial.points = 5;
+  // Punto inicial
+  biaxial.point[0].celsius = (int)temp_sensor.readObjectTempC();
+  biaxial.point[0].minutes = 0;
   // Calentar a 2ºC/min hasta 85ºC
-  biaxial.point[0].celsius = 85;
-  biaxial.point[0].minutes = (85 - sensor_celsius) * (1/2); // Temp difference * (1 min / 2 ºC)
-  // Mantener 85ºC 1.5h
   biaxial.point[1].celsius = 85;
-  biaxial.point[1].minutes = biaxial.point[0].minutes + 1.5 * 60;
+  biaxial.point[1].minutes = (85 - biaxial.point[0].celsius) * (1/2); // Temp difference * (1 min / 2 ºC)
+  // Mantener 85ºC 1.5h
+  biaxial.point[2].celsius = 85;
+  biaxial.point[2].minutes = biaxial.point[0].minutes + 1.5 * 60;
   // Calentar 2ºC/min hasta 120ºC
-  biaxial.point[2].celsius = 120;
-  biaxial.point[2].minutes = biaxial.point[1].minutes + (biaxial.point[2].celsius - biaxial.point[1].celsius) * (1/2); // Temp difference * (1 min / 2 ºC)
-  // Mantener 120ºC 1h
   biaxial.point[3].celsius = 120;
-  biaxial.point[3].minutes = biaxial.point[2].minutes + 60;
+  biaxial.point[3].minutes = biaxial.point[1].minutes + (biaxial.point[2].celsius - biaxial.point[1].celsius) * (1/2); // Temp difference * (1 min / 2 ºC)
+  // Mantener 120ºC 1h
+  biaxial.point[4].celsius = 120;
+  biaxial.point[4].minutes = biaxial.point[2].minutes + 60;
 }
 
 void loop() {
@@ -163,7 +195,6 @@ void loop() {
   // Read the mode from the nextion display
   mode = display.readNumber("cb0.val");
 
-  target_celsius = display.readNumber("n0.val");  // Solo lo hacemos en el setup se ajusta automaticamente a 90
   display.writeNum("sw0.val", 0);
 
   // IF TOGGLE ON BUT TIMER OFF -> SET THE TIMER -> STARTS A TASK
@@ -172,48 +203,23 @@ void loop() {
       case 0:  // MANUAL MODE - SET TARGET TEMPERATURE AND TIME UNTIL OFF.
         auto manual_mode_task = timer.every(60000, manual_mode); // Update the time every DELAY in miliseconds, then the function to call periodically
         break;
-      case 1:  // CURVAS
+      case 1:  // BIAXIAL CURVE
         auto curve_mode_task = timer.every(60000, curve_mode); // Update the time every DELAY in miliseconds, then the function to call periodically
         break;
     }
   }
 }
 
+int interpolate(int t, int t1, int T1, int t2, int T2){
+  /*
+  t is the current time, t1 and T1 the time and temperature of the first point, t2 and T2 the same for the next point
+
+  Returns the temperature that corresponds to the time t between those points.
+  */
+  return (int)(T1 + (t-t1)/(t2-t1) * (T2 - T1));
+}
+
 // TODO SOMETHING BLINKING ON THE SCREEN TO KNOW IT'S NOT FROZEN
+// TODO CHECK RIGHT POLARITY OF RELAY SIGNAL, IF DISCONNECTED -> OFF
 
-
-
-//////////////
-// /*Indicar programa*/
-// programaSeleccionado = display.readNumber("cb1.val");
-
-// display.writeNum("n2.val", programas[programaSeleccionado].tempObj);
-// display.writeNum("n3.val", programas[programaSeleccionado].progresion);
-// display.writeNum("n4.val", programas[programaSeleccionado].tiempo);
-
-
-// if (display.readNumber("sw0.val") == 1) {
-
-//   if (timer.isStopped() == true)
-//   {
-//     timer.start();
-//     minutes_left = programas[programaSeleccionado].tiempo;
-//     target_celsius = sensor_celsius;
-//   }
-//   timer.update();
-//   display.writeNum("n0.val", minutes_left);
-//   display.writeNum("n5.val", target_celsius);
-//   if (minutes_left > 0) set_heater(target_celsius);
-//   else {
-//     display.writeNum("sw0.val", 0);
-//     digitalWrite(RELAY_PIN, RELAY_OFF);
-//   }
-// }
-// else {
-//     //set_heater(0); //Desactivamos el calefactor por seguridad
-//     timer.stop();
-//     digitalWrite(RELAY_PIN, RELAY_OFF);
-//   }
-
-// break;
 
