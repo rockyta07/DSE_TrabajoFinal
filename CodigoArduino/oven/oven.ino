110663d5b7e212c9f9694e1556bf7494f3ba2329
Rubén <ruben.jimenezmejias@gmail.com>
2023-04-12 01:41:27 +0200

    Add files via upload

--- CodigoArduino/oven/oven.ino
+++ CodigoArduino/oven/oven.ino
@@ -1,14 +1,25 @@
 #include <EasyNextionLibrary.h>
 #include <trigger.h>
 #include <Adafruit_MLX90614.h>
-#include <arduino-timer.h> //#include <TimerOne.h> //#include <timer.h> #<timeLib.h>
+#include "TimeLib.h" // arduino-timer makes the arduino nano crash with no errors, and restarts infinitely. //#include <TimerOne.h> //#include <timer.h> #<timeLib.h>
 
 
 // CONFIGURATION CONSTANTS
-#define RELAY_PIN 13  // Signal pin to the relay. 5V = OFF = CUT, 0V = ON = ALLOW
+#define RELAY_PIN 2  // Signal pin to the relay. 5V = OFF = CUT, 0V = ON = ALLOW
+#define BEEPER_RELAY 10
 //#define HEATER_DELAY   // Delay in miliseconds (60000 means every minute)
+#define HEART_LED 13
+bool heart_led_on = false;
 #define RELAY_OFF HIGH
 #define RELAY_ON LOW // Hay que dar tierra
+
+/// Thermocouple
+#include <SPI.h>
+#include "Adafruit_MAX31855.h"
+#define MAXDO   3
+#define MAXCS   4
+#define MAXCLK  5
+Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
 /*
 Update the display at least every second
@@ -32,7 +43,7 @@ where {name} is:
 - Toggle (Switch, to turn on, starts the timer for the curve): toggle
 
 Show:
-- actual temp: temp
+- actual temp: sensor
 - target temp: target
 - elapsed time: elapsed
 - remaining time: remaining
@@ -49,7 +60,7 @@ struct point {
   long minutes; // Target time in seconds
 };
 
-#define MAX_POINTS
+#define MAX_POINTS 12
 
 struct curve { // Name and Array of time/temperature points to follow
   String name; // Name of the curve
@@ -57,17 +68,27 @@ struct curve { // Name and Array of time/temperature points to follow
   point point[MAX_POINTS]; // Array with points to follow
 };
 
-// Create timer with ms resolution
-auto timer = timer_create_default();  // auto means deduce variable type
-
 int sensor_celsius;
-int mode = 1; // 0: manual,
+int mode = 2; // 0: manual, 1: ciclo manual, 2: curvas
 int programaSeleccionado; // TODO REMOVE, EACH PROGRAM IS A MODE. SIMPLER.
 int target_celsius;
 
 curve cur;
 
 ////////////////////////////////// FUNCTIONS //////////////////////////////////
+void beep() {
+  for (int i=0; i < 8; i++){
+    for (int j=0; j < 20; j++){
+      // 1/10ms = 200Hz sound
+      digitalWrite(BEEPER_RELAY, HIGH);
+      delay(5);
+      digitalWrite(BEEPER_RELAY, LOW);
+      delay(5);
+    }
+    delay(200);
+  }
+}
+
 void set_heater(int target_celsius) {
   /*
   Turns the resistor ON or OFF depending on the temperature.
@@ -85,17 +106,22 @@ void set_heater(int target_celsius) {
     digitalWrite(RELAY_PIN, RELAY_OFF);
 }
 
-void temp_watchdog(int celsius) {
-  /*
-  IF VALUE OUT OF NORMAL RANGE, CUT RELAY, ERROR IN DISPLAY, sleep 1 second
+int read_temperature() {
+  /* Reads the temperature of the thermocouple and checks everything OK.
   */
-
-  if ((celsius <= 0) || (celsius > 200)) {
-    display.writeStr("page 2");
-    digitalWrite(RELAY_PIN, RELAY_OFF);
-    delay(1000);
-  } else if (celsius == NAN){
-    exception("Fallo en el sensor de temperatura",false);
+  int sensor_celsius = thermocouple.readCelsius();
+  
+  if (isnan(sensor_celsius)) {
+    // Serial.println("Thermocouple fault(s) detected!");
+    // uint8_t e = thermocouple.readError();
+    // Serial print error with more details
+    // if (e & MAX31855_FAULT_OPEN) Serial.println("FAULT: Thermocouple is open - no connections.");
+    // if (e & MAX31855_FAULT_SHORT_GND) Serial.println("FAULT: Thermocouple is short-circuited to GND.");
+    // if (e & MAX31855_FAULT_SHORT_VCC) Serial.println("FAULT: Thermocouple is short-circuited to VCC.");
+    // Show error in display and disconnect relay
+    exception("Thermocouple error, can't read temperature.", false);
+  } else {
+    return sensor_celsius;
   }
 }
 
@@ -103,21 +129,26 @@ void exception(String error_string, bool stop) {
   /*
   Prints error_string in the nextion display
   */
+  digitalWrite(RELAY_PIN, RELAY_OFF);
   display.writeStr("page 3");
   display.writeStr("t1.txt", error_string);
-  digitalWrite(RELAY_PIN, RELAY_OFF);
-  delay(1000);
+  beep();
+  delay(2000);
   if (stop) {
-    while (1);
+    while (1){
+      beep();
+    }
   }
-  
 }
 
 void manual_mode() {
   /*
   Updates the time variable in code and the display
   TODO SEPARATE minutes_left--; from this function, so this one can go fast?
+
+  
   */
+  /*
   static long minutes_left = display.readNumber("remaining.val");
   minutes_left--;
 
@@ -125,74 +156,98 @@ void manual_mode() {
     // TIME ENDED.
     digitalWrite(RELAY_PIN, RELAY_OFF); // Heater OFF
     display.writeNum("toggle.val", 0); // Display toggle OFF
-    timer.cancel(); // End all the tasks of the timer
     display.writeNum("remaining.val", minutes_left);
   } else {
     // STILL WORK TO DO
     display.writeNum("remaining.val", minutes_left);
-    target_celsius = display.readNumber("temp.val");
+    target_celsius = display.readNumber("sensor.val");
     set_heater(target_celsius);
   }
+  */
 }
 
 void curve_mode() {
   /*
   Starts reading the curve and going linearly from point to point as time goes on.
   TODO PASS A CURVE TO MAKE THIS FUNCTION GENERIC, instead of using cur directly
+
+  Depending on the value of toggle.val, it will start or stop the process of following the curve.
   */
-  static long minutes_passed = 0;
+  static int toggle_on = 0;
+  static int toggle_previous_on = 0;
 
-  // If time passed > than time of final point
-  if (minutes_passed > cur.point[cur.points - 1].minutes) {
-    // TIME ENDED.
-    digitalWrite(RELAY_PIN, RELAY_OFF); // Heater OFF
-    display.writeNum("toggle.val", 0); // Display toggle OFF
-    display.writeNum("temp.val", target_celsius); // Display the temperature
-    display.writeNum("target.val", 0); // Display target of 0ºC
-    timer.cancel(); // End all the tasks of the timer
-  } else {
-    // Still work to do
-
-    // Find the next point to follow
-    int i = 0;
-    while (cur.point[i].minutes < minutes_passed) {
-      // When the point is in the future, exit the while loop
-      i++;
-    }
-
-    if (i == 0) {
-      exception("curve_mode ERROR. Either the code or the curve are wrong.", true);
-    }
-
-    // Now i is the next point
-    int target_celsius = interpolate(minutes_passed, cur.point[i-1].minutes, cur.point[i-1].celsius, cur.point[i].minutes, cur.point[i].celsius);
-    // TODO write minutes_passed or graph, GUI.
-    set_heater(target_celsius);
-    display.writeNum("temp.val", target_celsius); // Display the temperature
-    display.writeNum("target.val", target_celsius); // Display target of target_celsius
-  }
+  toggle_on = display.readNumber("toggle2.val"); // TODO READ THE RIGHT VALUE
+  int now_minutes = int(now()/60);
+  
+  Serial.println("");
+  Serial.print("Toggle: ");
+  Serial.println(toggle_on);
 
   // Update time
-  minutes_passed++;
-  display.writeNum("elapsed.val", minutes_passed);
-  display.writeNum("total.val", cur.point[cur.points-1]);
-  display.writeNum("remaining.val", cur.point[cur.points-1] - minutes_passed);
-  
+  display.writeNum("elapsed2.val", now_minutes);
+  display.writeNum("total2.val", cur.point[cur.points-1].minutes);
+  display.writeNum("remaining2.val", cur.point[cur.points-1].minutes - now_minutes);
+
+  // If just pressed the toggle
+  if (toggle_on && !toggle_previous_on) {
+    // Restart the curve
+    define_curve(); // To update the time
+  } else if (toggle_on) {
+    // Continue with the curve
+
+    // If time passed > than time of final point
+    if (now() > cur.point[cur.points - 1].minutes) {
+      // TIME ENDED.
+      digitalWrite(RELAY_PIN, RELAY_OFF); // Heater OFF
+      display.writeNum("toggle2.val", 0); // Turn OFF the toggle
+      display.writeNum("target2.val", -1); // Display target of -1ºC
+    } else {
+      // Still work to do
+
+      // Find the next point to follow
+      int i = 0;
+      while (cur.point[i].minutes < now_minutes) {
+        // When the point is in the future, exit the while loop
+        i++;
+      }
+
+      /*
+      if (i == 0) {
+        exception("curve_mode ERROR. Either the code or the curve are wrong.", true);
+      }
+      */
+
+      // i is the next point
+      int target_celsius = interpolate(now(), cur.point[i-1].minutes, cur.point[i-1].celsius, cur.point[i].minutes, cur.point[i].celsius);
+      // TODO write minutes_passed or graph, GUI.
+      set_heater(target_celsius);
+      display.writeNum("target.val", target_celsius); // Display target of target_celsius
+    }
+  }
+
+  toggle_previous_on = toggle_on;
 }
 
-void setup() {
-  display.begin(9600);
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, RELAY_OFF);
+int interpolate(int t, int t1, int T1, int t2, int T2) {
+  /*
+  t is the current time, t1 and T1 the time and temperature of the first point, t2 and T2 the same for the next point
 
-  Serial.begin(9600);
+  Returns the temperature that corresponds to the time t between those points.
+  */
+  return (int)(T1 + (t-t1)/(t2-t1) * (T2 - T1));
+}
 
-  ////////// CURVES ///////////
+void define_curve(){
+  /*
+  Defines the declared curve with the time when it's been called
+
+  */
   cur.name = "cur: 2ºC/min, 85ºC 1.5h, 120ºC 1h";
   cur.points = 6;
   // Punto inicial
   cur.point[0].celsius = sensor_celsius;
-  cur.point[0].minutes = 0;
+  cur.point[0].minutes = now();
+  
   // Calentar a 2ºC/min hasta 85ºC
   cur.point[1].celsius = 85;
   cur.point[1].minutes = cur.point[0].minutes + (85 - cur.point[0].celsius) * (1/2); // Temp difference * (1 min / 2 ºC)
@@ -210,47 +265,77 @@ void setup() {
   cur.point[5].minutes = cur.point[4].minutes + (cur.point[4].celsius - cur.point[5].celsius) * (1/2);
 }
 
-void loop() {
+void setup() {
+  display.begin(9600);
+  Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(HEART_LED, OUTPUT);
+  pinMode(BEEPER_RELAY, OUTPUT);
+
+  // Startup check and leave OFF
+  digitalWrite(RELAY_PIN, RELAY_ON);
+  delay(50);
   digitalWrite(RELAY_PIN, RELAY_OFF);
-  // Nextion touch response
-  display.NextionListen();
-  // Update timer status and check whether arrived at setpoint
-  timer.tick();
-  // Read sensor temperature
-  sensor_celsius = 25; // TODO THERMOCOUPLE
-  display.writeNum("n1.val", (uint32_t)sensor_celsius);
-  // If crazy, error
-  temp_watchdog(sensor_celsius);
-  // Read the mode from the nextion display
-  mode = display.readNumber("mode.val");
-
-  display.writeNum("toggle.val", 0);
-
-  // IF TOGGLE ON BUT TIMER OFF -> SET THE TIMER -> STARTS A TASK
-  Serial.println(display.readNumber("toggle.val") == 1);
-  Serial.println(timer.empty());
-  if ((display.readNumber("toggle.val") == 1) && timer.empty()) {
-    switch (mode) {
-      case 0:  // MANUAL MODE - SET TARGET TEMPERATURE AND TIME UNTIL OFF.
-        auto manual_mode_task = timer.every(60000, manual_mode); // Update the time every DELAY in miliseconds, then the function to call periodically
-        break;
-      case 1:  // cur CURVE
-        auto curve_mode_task = timer.every(60000, curve_mode); // Update the time every DELAY in miliseconds, then the function to call periodically
-        break;
-    }
-  } else if (display.readNumber("toggle.val") == 0) {
-    // If toggle OFF
-    timer.cancel(); // End all the tasks of the timer. Interrupt any works in progress.
+  delay(50);
+  
+  if (!thermocouple.begin()) {
+    Serial.println("Thermocouple error.");
+    exception("ERROR: Sensor de temperatura no se pudo inicializar",true);
   }
+  
+  sensor_celsius = read_temperature();  
+  Serial.print("Temperature: ");
+  Serial.println(sensor_celsius);
+
+
+  setTime(0);
+  ////////// CURVES ///////////
+  define_curve();
+
+  display.writeNum("sensor_temp2.val",sensor_celsius);
+  display.writeNum("target2.val",-1);
+  display.writeNum("remaining2.val",0);
+
 }
 
-int interpolate(int t, int t1, int T1, int t2, int T2) {
-  /*
-  t is the current time, t1 and T1 the time and temperature of the first point, t2 and T2 the same for the next point
+void loop() {
+  // Heart led toggle
+  digitalWrite(13, heart_led_on);
+  heart_led_on = !heart_led_on;
 
-  Returns the temperature that corresponds to the time t between those points.
-  */
-  return (int)(T1 + (t-t1)/(t2-t1) * (T2 - T1));
+  // Nextion touch response
+  display.NextionListen();
+  
+  // Read sensor temperature
+  sensor_celsius = thermocouple.readCelsius();
+  if ((sensor_celsius <= -20) || (sensor_celsius > 200) || (sensor_celsius == NAN)) {
+    exception("Temperatura fuera de rango o NAN",false);
+  }
+  Serial.print("T [ºC]: ");
+  Serial.println(sensor_celsius);
+
+// DELETEME  display.writeNum("target.val", sensor_celsius);
+  display.writeNum("sensor_temp2.val", sensor_celsius);
+  //display.writeNum("elapsed2.val", now()/60);
+  
+  // Read the mode from the nextion display
+  mode = display.readNumber("mode_val.val"); // TODO READ RELIABLY
+  
+  Serial.print("Mode: ");
+  Serial.println(mode);
+  switch (mode) {
+    case 0:
+      manual_mode();
+      break;
+    case 1:
+      //curvas();
+      break;
+    case 2:
+      curve_mode();
+      break;
+  }
+  
+
 }
 
 // TODO SOMETHING BLINKING ON THE SCREEN TO KNOW IT'S NOT FROZEN
